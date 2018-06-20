95960e4e894e83e21102a486d444b48e7d47f1df
Yang09701194 <mustnecessity@gmail.com>
2018-06-20 17:52:58 +0800

    test and prove workable, thanks for the answer: https://stackoverflow.com/a/34384189/4573839

---
+++ TestKeyHook/TestKeyHook/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SnagFree.TrayApp.Core;
+
+namespace TestKeyHook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            SetupKeyboardHooks();
+
+        }
+
+
+        private GlobalKeyboardHook _globalKeyboardHook;
+
+        public void SetupKeyboardHooks()
+        {
+            _globalKeyboardHook = new GlobalKeyboardHook();
+            _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+        }
+
+        private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            //Debug.WriteLine(e.KeyboardData.VirtualCode);
+
+            if (e.KeyboardData.VirtualCode != GlobalKeyboardHook.VkSnapshot)
+                return;
+
+            // seems, not needed in the life.
+            //if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.SysKeyDown &&
+            //    e.KeyboardData.Flags == GlobalKeyboardHook.LlkhfAltdown)
+            //{
+            //    MessageBox.Show("Alt + Print Screen");
+            //    e.Handled = true;
+            //}
+            //else
+
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
+            {
+                MessageBox.Show("Print Screen");
+                e.Handled = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            _globalKeyboardHook?.Dispose();
+        }
+
+    }
+}
